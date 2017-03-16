4f633aeb96591601d8affcdf4c11a1e6d95fb76f
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2017-03-16 11:07:25 +0200

    working on password login

--- src/main/java/eu/organicity/client/OrganicityServiceBaseClient.java
+++ src/main/java/eu/organicity/client/OrganicityServiceBaseClient.java
@@ -2,20 +2,49 @@ package eu.organicity.client;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 public class OrganicityServiceBaseClient {
-
+    
     protected final RestTemplate restTemplate;
+    private final String client_id;
+    private final String client_secret;
+    private final String username;
+    private final String password;
+    private final JwtToken jwtToken;
     protected String token;
     protected HttpHeaders headers;
     protected HttpEntity<String> req;
-
+    
     public OrganicityServiceBaseClient() {
         this("");
     }
-
+    
+    
+    public OrganicityServiceBaseClient(final String client_id, final String client_secret, final String username, final String password) {
+        restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        restTemplate.setErrorHandler(new CustomResponseErrorHandler());
+        headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
+        req = new HttpEntity<>("", headers);
+        
+        this.token = null;
+        
+        this.client_id = client_id;
+        this.client_secret = client_secret;
+        this.username = username;
+        this.password = password;
+        
+        jwtToken = doGetToken();
+        headers.remove(HttpHeaders.AUTHORIZATION);
+        headers.add(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", jwtToken.getAccess_token()));
+    }
+    
     public OrganicityServiceBaseClient(final String token) {
         this.token = token;
         restTemplate = new RestTemplate();
@@ -28,17 +57,45 @@ public class OrganicityServiceBaseClient {
             headers.add(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", token));
         }
         req = new HttpEntity<>("", headers);
+        
+        this.client_id = null;
+        this.client_secret = null;
+        this.username = null;
+        this.password = null;
+        this.jwtToken = null;
     }
-
+    
+    public JwtToken doGetToken() {
+        
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "password");
+        params.add("client_id", client_id);
+        params.add("client_secret", client_secret);
+        params.add("username", username);
+        params.add("password", password);
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded");
+        
+        HttpEntity<MultiValueMap<String, String>> req = new HttpEntity<>(params, headers);
+        
+        
+        JwtToken response = restTemplate.exchange(
+                "https://accounts.organicity.eu/realms/organicity/protocol/openid-connect/token",
+                HttpMethod.POST, req, JwtToken.class).getBody();
+        
+        return response;
+    }
+    
     public void clearToken() {
         this.token = "";
         headers.remove(HttpHeaders.AUTHORIZATION);
     }
-
+    
     public String getToken() {
         return token;
     }
-
+    
     public void setToken(final String token) {
         this.token = token;
         if (!"".equals(token)) {
